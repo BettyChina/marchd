763997037f80facfa8f47ce8149d446a738939c9
BettyChina <34248486+BettyChina@users.noreply.github.com>
2018-03-28 00:41:07 -0700

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,7 @@
+
+from django.conf.urls import url, include # Notice we added include
+from django.contrib import admin
+urlpatterns = [
+      url(r'^', include('apps.dojo_ninjas.urls')) # And now we use the include function to pull in our first_app.urls...
+      
+  ]
